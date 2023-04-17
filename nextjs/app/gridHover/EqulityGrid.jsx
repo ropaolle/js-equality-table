5d69728e0ac95b78132d975f0bd45e8b0d8c8fc1
ropaolle <ropaolle@gmail.com>
2023-04-17 13:12:24 +0200

    grid

--- nextjs/app/gridHover/EqulityGrid.jsx
+++ nextjs/app/gridHover/EqulityGrid.jsx
@@ -1,58 +1,47 @@
 'use client';
-import { useRef, Fragment } from 'react';
+import {} from 'react';
 import styles from './EqulityGrid.module.css';
 import { equality, values } from '../../lib/jsEqulity';
 
 export default function EqulityGrid() {
-  const myRef = useRef(null);
-
-  const onClick = () => {
-    console.log('myRef.current', myRef.current);
-  };
-
   const getSign = ({ x, y }) => equality({ x, y }, ['=', '≅', '≠']);
   const getStyle = ({ x, y }) => equality({ x, y }, [styles.strict, styles.loose, styles.notEqual]);
-  const getTitle = ({ x, y }) => equality({ x, y }, ['Strict equality', 'Loose equality', 'Not equal']);
+  const getTitle = (x, y, xIndex, yIndex) => {
+    const sign = getSign({ x, y });
+    const title = equality({ x, y }, ['Strict equality', 'Loose equality', 'Not equal']);
+    return `${values[xIndex].label} ${sign} ${values[yIndex].label} (${title})`;
+  };
 
-  // const getTitle2 = ({ x, y }) => {
-  //   const sign = getSign({x,y})
-
-  // }
-
-  const header = () => (
-    <>
-      <div></div>
-      {values.map(({ label }, index) => (
-        <div className={styles.labelCol} key={index + 'label'} id={index + 'label'}>
-          {label}
-        </div>
-      ))}
-    </>
-  );
-
-  const rows = () =>
-    values.map(({ x, label }, indexX) => (
-      <Fragment key={indexX}>
-        <div ref={myRef} key={indexX} className={styles.labelRow} onClick={onClick}>
-          {label}
-        </div>
-        {values.map(({ y }, indexY) => (
-          <div
-            key={indexY}
-            className={getStyle({ x, y })}
-            // className={[styles.cell, getStyle({ x, y })].join(' ')}
-            title={getTitle({ x, y })}
-          >
-            {getSign({ x, y })}
-          </div>
-        ))}
-      </Fragment>
+  const header = () =>
+    ['', ...values].map(({ label }, index) => (
+      <div className={styles.headerLabel} key={index}>
+        {label}
+      </div>
     ));
 
+  const sidebar = () =>
+    values.map(({ label }, index) => (
+      <div className={styles.sidebarLabel} key={index}>
+        {label}
+      </div>
+    ));
+
+  const rows = () =>
+    values.map(({ x }, xIndex) =>
+      values.map(({ y }, yIndex) => (
+        <div key={yIndex} className={getStyle({ x, y })} title={getTitle(x, y, xIndex, yIndex)}>
+          {getSign({ x, y })}
+        </div>
+      ))
+    );
+
   return (
-    <div className={styles.table}>
-      {header()}
-      {rows()}
+    <div className={styles.grid}>
+      <div className={styles.header}>{header()}</div>
+      <div className={styles.sidebar}>{sidebar()}</div>
+      <div /* className={styles.content} */>
+        <div className={styles.table}>{rows()}</div>
+      </div>
     </div>
   );
 }
