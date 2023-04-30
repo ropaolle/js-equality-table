b64904c258257e3caaa2e58e3c333e61f6e538a8
ropaolle <ropaolle@gmail.com>
2023-04-30 13:00:02 +0200

    slow

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,27 @@ const values = [
   { label: "[0]", x: [0], y: [0] },
   { label: "[1]", x: [1], y: [1] },
   { label: "NaN", x: NaN, y: NaN },
+  //   
+  { label: "false", x: false, y: false },
+  { label: "1", x: 1, y: 1 },
+  { label: "0", x: 0, y: 0 },
+  { label: "-1", x: -1, y: -1 },
+  { label: '"true"', x: "true", y: "true" },
+  { label: '"false"', x: "false", y: "false" },
+  { label: '"1"', x: "1", y: "1" },
+  { label: '"0"', x: "0", y: "0" },
+  { label: '"-1"', x: "-1", y: "-1" },
+  { label: '""', x: "", y: "" },
+  { label: "null", x: null, y: null },
+  { label: "undefined", x: undefined, y: undefined },
+  { label: "Infinity", x: Infinity, y: Infinity },
+  { label: "-Infinity", x: -Infinity, y: -Infinity },
+  { label: "[]", x: [], y: [] },
+  { label: "{}", x: {}, y: {} },
+  { label: "[[]]", x: [[]], y: [[]] },
+  { label: "[0]", x: [0], y: [0] },
+  { label: "[1]", x: [1], y: [1] },
+  
 ];
 
 export default function App() {
